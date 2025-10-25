a20146ba274b75947d5f11e917b9c39a330c25d1
Naiel-Yohannes <naielyohannes@gmail.com>
2025-10-25 04:22:57 -0700

    Making the hero sections responsive

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,3 +1,4 @@
+import './Home.css'
 import Flying from './Flying'  
 import Places from './Places'  
 import Backpacking from './Backpacking'
@@ -7,28 +8,13 @@ import Footer from './Footer'
 function Home(){
     const homeStyles = {
         backgroundImage: `linear-gradient(90deg, rgba(15, 55, 68, 0.8) 0%, rgba(173, 216, 230, 0) 100%), url("https://t3.ftcdn.net/jpg/06/46/20/52/360_F_646205247_k4zVBz5tdHv5EUTfy56JxsTmB5nRip5c.jpg")`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        height: '85vh',
-        backgroundPosition: 'center',
-        backgroundAttachment: 'fixed',
-        color: 'white'
-    
-    };
-
-    const heroContentStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        height: '70%',
-        marginLeft: '8%'
     };
 
     return(
         <>
-        <section style={homeStyles}>
-            <div style={heroContentStyle}>
-                <h1 style={{fontSize: "48px", width: "40%"}}>Make your travels whishlist, We'll do the rest</h1>
+        <section style={homeStyles} className='hero'>
+            <div className='hero-content'>
+                <h1>Make your travels whishlist, We'll do the rest</h1>
                 <p>Special offers to suit your plans</p>
             </div>
         </section>
--- src/Components/Stays.jsx
+++ src/Components/Stays.jsx
@@ -1,3 +1,4 @@
+import './Home.css'
 import Searches from './Searches'
 import FlyingStays from './FlyingStays' 
 import Places from './Places'  
@@ -7,28 +8,13 @@ import Footer from './Footer'
 function Stays(){
     const staysStyles = {
         backgroundImage: `linear-gradient(90deg, rgba(15, 55, 68, 0.8) 0%, rgba(173, 216, 230, 0) 100%), url("https://cdn.shopify.com/s/files/1/0080/0984/2786/files/shutterstock_1181523733.jpg?v=1655221599")`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        height: '85vh',
-        backgroundPosition: 'center',
-        backgroundAttachment: 'fixed',
-        color: 'white'
-    
-    };
-
-    const staysContentStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        height: '70%',
-        marginLeft: '8%'
     };
 
     return(
         <>
-        <section style={staysStyles}>
-            <div style={staysContentStyle}>
-                <h1 style={{fontSize: "48px", width: "40%"}}>Make your travels whishlist, We'll do the rest</h1>
+        <section className='hero' style={staysStyles}>
+            <div className='hero-content'>
+                <h1>Make your travels whishlist, We'll do the rest</h1>
                 <p>Special offers to suit your plans</p>
             </div>
         </section>
